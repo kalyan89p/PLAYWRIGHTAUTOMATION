717474eb650c8d2d9e6601b456e4a0877942215a
kalyan89p <kalyan89p@gmail.com>
2026-06-15 07:34:16 +0530

    api test cases completed

--- tests/K/api.spec.ts
+++ tests/K/api.spec.ts
@@ -11,20 +11,19 @@
 // PUT to local server - modify data on existing id.  - aipoindi
 // check 200.  - aipoindi
 // get using id , verify the changes are reflected.  - aipoindi
-// DELETE FROM local server
-// check 200. 
-// get using id , verify the RECORD IS DELETED.
-// DELETE FROM local server
-// check 200. 
-// get using id , verify the RECORD IS DELETED.
+// DELETE FROM local server    - aipoindi
+// check 200.   - aipoindi
+// get using id , verify the RECORD IS DELETED.  - aipoindi
+// DELETE FROM local server  - aipoindi
+// check 200.   - aipoindi
+// get using id , verify the RECORD IS DELETED.  - aipoindi
 
 import { test, expect } from '@playwright/test';
 let localhost = 'http://localhost:3000/'
 let usersUrl = "http://localhost:3000/users"
 let id1: string;
 let id2: string;
-let id1Url: string;
-let id2Url: string;
+
 
 test('GET FROM API', async ({ request }) => {
     const response = await request.get(localhost)
@@ -49,7 +48,6 @@ test('POST TO API 1', async ({ request }) => {
     //console.log(response.json())
     const responseBody = await response2.json();
     id1 = responseBody.id;
-    id1Url = usersUrl+"/"+id1;
     console.log("object Id = " + id1)
 });
 
@@ -74,21 +72,18 @@ test('POST TO API2', async ({ request }) => {
     //console.log(response.json())
     const responseBody3 = await response3.json();
     id2 = responseBody3.id;
-    id2Url = usersUrl+"/"+id2;
     console.log("object Id = " + id2)
-    //console.log(responseBody3)
 });
 
 
-test.only('PATCH TO ID1 ', async ({ request }) => {
+test('PATCH TO ID1 ', async ({ request }) => {
     //const response = await request.get(localhost)
     //console.log(response)
- const response4 = await request.patch(id1Url, {
+ const response4 = await request.patch(usersUrl+"/"+id1, {
         data: {
              name: "Kalyan"
         }
-    }
-    );
+    });
 
     expect(response4.status()).toBe(200);
 
@@ -103,7 +98,7 @@ test.only('PATCH TO ID1 ', async ({ request }) => {
 test('PUT TO ID2 ', async ({ request }) => {
     //const response = await request.get(localhost)
     //console.log(response)
- const response5 = await request.put(id2Url, {
+ const response5 = await request.put(usersUrl+"/"+id2, {
         data: {
         	"name": "Macharla",
 	    "data": {
@@ -124,4 +119,26 @@ test('PUT TO ID2 ', async ({ request }) => {
             console.log("test case 5 passed");
         }
   
+});
+
+
+test('DELETE ID1 ', async ({ request }) => {
+    //const response = await request.get(localhost)
+    //console.log(response)
+ let response6 = await request.delete(usersUrl+"/"+id1);
+ const response6Body = await response6.json();
+ expect(response6.status()).toBe(200);  
+ response6 = await request.get(usersUrl+"/"+id1);
+ expect(response6.status()).toBe(404);
+
+});
+test('DELETE ID2 ', async ({ request }) => {
+    //const response = await request.get(localhost)
+    //console.log(response)
+ let response7 = await request.delete(usersUrl+"/"+id2);
+ const response7Body = await response7.json();
+ expect(response7.status()).toBe(200);  
+ response7 = await request.get(usersUrl+"/"+id1);
+ expect(response7.status()).toBe(404);
+
 });
