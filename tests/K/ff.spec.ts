809152aa9b9fc82ed377a06020657fc6c47f1b2a
kalyan89p <kalyan89p@gmail.com>
2026-06-17 00:05:47 +0530

    read from excel passed

--- tests/K/ff.spec.ts
+++ tests/K/ff.spec.ts
@@ -1,43 +1,47 @@
-import {test,expect,request} from '@playwright/test';
-import { execFile } from 'node:child_process';
-import { json } from 'node:stream/consumers';
+import { test, expect } from '@playwright/test';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as ExcelJS from 'exceljs';
 
+const url = 'https://testautomationpractice.blogspot.com/';
+const nameL = "//*[@id='name']";
 
+const jsonData = fs.readFileSync('./tests/k/sample.json', 'utf-8');
+const jfile = JSON.parse(jsonData);
 
-let url = 'https://testautomationpractice.blogspot.com/'
-let nameL = "//*[@id='name']"
-const jsonData = fs.readFileSync('./tests/k/sample.json','utf-8');
-const jfile = JSON.parse(jsonData)
-const name = jfile.user[1].name
-const id = jfile.user[1].id
+const name = jfile.user[1].name;
+const id = jfile.user[1].id;
 
+test.only('Excel Testcase', async ({ page }) => {
+    const workbook = new ExcelJS.Workbook();
+
+    const filePath = path.resolve(__dirname, '../k/try.xlsx');
+
+    await workbook.xlsx.readFile(filePath);
+
+    const sheet = workbook.getWorksheet(1);
+
+    if (!sheet) {
+        throw new Error('Worksheet not found');
+    }
+
+    // Row numbers start from 1
+    const row2 = sheet.getRow(2);
+
+    // Column B = 2
+    const username = row2.getCell(2).text;
+
+    console.log('Username:', username);
+
+    // Example usage
+   await page.goto(url);
+    await page.locator(nameL).fill(username);
+
+    await expect(page.locator(nameL)).toHaveValue(username);
+});
 
 test('Json Test Case', async({page,request}) =>{
 await page.goto(url)
   await page.locator(nameL).fill(name);
   await page.waitForTimeout(5000)
-});
-
-
-
-test.only('Excel Testcase', async({page,request}) =>{
- const workbook = new ExcelJS.Workbook();
-  const filePath = path.resolve(__dirname, '../try.xlsx');
-  await workbook.xlsx.readFile(filePath);
-  const sheet = workbook.getWorksheet(1);
-  const row2 = sheet.getRow(2);
-  const username = row2.getCell(2).text;
-
-  if (!sheet) {
-  throw new Error('Sheet $ $ $ $. not found');
-  }
-
-
-  console.log(username);
-  //await page.goto(url)
-  
-
 });
